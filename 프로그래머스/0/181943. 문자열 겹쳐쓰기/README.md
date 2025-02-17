7e8691a679057cc889afe65ab4ff684d0ddf4aff
Na Wonji <71624596+rowanna@users.noreply.github.com>
2025-02-17 11:43:38 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.35 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 17일 11:34:33
+2025년 02월 17일 11:43:37
 
 ### 문제 설명
 
--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,16 +1,5 @@
 function solution(my_string, overwrite_string, s) {
     let answer = '';
-    const newString = [...my_string]
-    const newOverwriteString = [...overwrite_string]
-    for(let i = s; i <= s + overwrite_string.length; i++ ){
-        
-        if(i === s + overwrite_string.length || !newString[i]) {
-            
-        }else {
 
-            newString[i] = newOverwriteString[i-s]
-        }
-    }
-    answer = newString.join('')
-    return answer;
+    return my_string.slice(0,s)+overwrite_string + my_string.slice(s+overwrite_string.length);
 }
