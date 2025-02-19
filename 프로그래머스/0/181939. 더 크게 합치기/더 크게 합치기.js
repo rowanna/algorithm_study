eb3c996f443eeff25e6a1ff70859a0106a1b047d
Na Wonji <71624596+rowanna@users.noreply.github.com>
2025-02-19 10:13:02 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,6 +1,5 @@
 function solution(a, b) {
     var answer = 0;
-    const newA = String(a)
-    const newB = String(b)
-    return newA + newB >= newB + newA ? Number(newA+newB) : Number(newB+newA) ;
+    
+    return Math.max(`${a}${b}`, `${b}${a}`)
 }
