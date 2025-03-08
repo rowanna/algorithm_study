e6fa1737e6ddabc3ada89b5a28f8faa747d97731
Na Wonji <71624596+rowanna@users.noreply.github.com>
2025-03-08 18:30:04 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let odd = '';
+    let even = '';
+    for(let i = 0; i < num_list.length; i++) {
+        if(num_list[i] % 2 === 0) even += num_list[i]
+        if(num_list[i] % 2 !== 0) odd += num_list[i]
+        
+    }
+    return Number(odd) + Number(even);
+}
