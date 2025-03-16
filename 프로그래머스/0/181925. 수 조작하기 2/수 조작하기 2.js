5ffafc6f498588be46eadd2c6636c05019f5689f
Na Wonji <71624596+rowanna@users.noreply.github.com>
2025-03-16 23:15:07 +0900

    [level 0] Title: 수 조작하기 2, Time: 30.97 ms, Memory: 45.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,18 @@
+function solution(numLog) {
+    let answer = '';
+    numLog.forEach((num,idx) => {
+        if(idx>0) {
+            switch(num - numLog[idx-1]) {
+                case 1: answer+='w'
+                break;
+                case -1: answer+='s'
+                break;
+                case 10: answer+='d'
+                break;
+                case -10: answer+='a'
+                break;
+            }
+        }
+    })
+    return answer;
+}
