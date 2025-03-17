f951bb47700524aaec6b20ba302f55ef1da016a8
Na Wonji <71624596+rowanna@users.noreply.github.com>
2025-03-17 22:50:20 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+    let answer = 0;
+    if(a%2===1 && b%2===1) {answer += (a*a) + (b*b)}
+    else if(a%2===1 || b%2===1) { answer += 2 * (a + b) }
+    else if(a%2===0 && b%2===0){answer += Math.abs(a - b) }
+    return answer;
+}
