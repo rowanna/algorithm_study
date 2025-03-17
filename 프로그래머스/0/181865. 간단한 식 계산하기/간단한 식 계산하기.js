0a102d47366f4cb5c7aa0028ed51ab789fbc2c4e
Na Wonji <71624596+rowanna@users.noreply.github.com>
2025-03-17 22:41:22 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ')
+    const convert = {
+        '+': ()=>  (+a) + (+b),
+        '-': ()=>  (+a) - (+b),
+        '*': ()=> (+a) * (+b),
+    }
+    return convert[op]();
+}
